dbbca18a1f9acaaf618572b7d656366656ec56de
Ken Lomax <ken.lomax@sap.com>
2019-05-17 10:09:49 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+ 
+ 
 import { AppComponent } from './app.component';
-
+import { HelloComponent } from './hello.component';
+import { ConfigModule } from '@spartacus/core';
+import { StorefrontModule, translations, defaultCmsContentConfig } from '@spartacus/storefront';
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, StorefrontModule.withConfig ({
+  site: {
+    baseSite: 'electronics-spa'   
+  }, i18n: {
+    resources: translations
+  }
+}),
+ConfigModule.withConfigFactory(defaultCmsContentConfig)
+ ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
